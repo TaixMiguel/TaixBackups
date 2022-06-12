2d40151e37c900fcefbf44c5bd9a6b89e90bba46
Miguelín <unbikercualquiera@gmail.com>
2022-06-12 17:34:32 +0200

    [ADDED] Obtención identificador directorio MEGA
    Se obtiene el directorio de MEGA al que subir el backup

--- app/backup/backupCreatorMega.py
+++ app/backup/backupCreatorMega.py
@@ -11,6 +11,32 @@ class MegaBackupCreator(BackupCreator):
     def __init__(self, source_dir: str, destination_dir: str, filename_backup: str):
         super().__init__(source_dir=source_dir, destination_dir=destination_dir, filename_backup=filename_backup)
 
+    def get_id_folder(self, mega: Mega) -> str:
+        # TODO: añadir línea al log (debug)
+        print(f"Se busca el directorio {self.destination_dir} en la cuenta de Mega")
+        folders = self.destination_dir.split("/")
+        parent_id: str = ''
+        path: str = ''
+        for i, folder in enumerate(folders):
+            if not folder and i == 0:
+                continue
+
+            # TODO: añadir línea al log (debug)
+            print(f"Buscando el directorio {folder}")
+            folder_find = mega.find(folder, exclude_deleted=True)
+            # TODO: añadir línea al log (debug)
+            print(folder_find)
+            if folder_find and (not parent_id or parent_id == folder_find[1].get('p')):
+                # TODO: añadir línea al log (debug)
+                print(f"Id padre {parent_id} - Soy {folder_find[0]}")
+                parent_id = folder_find[0]
+                path += folder + "/"
+            else:
+                # TODO: añadir línea al log (debug)
+                print(f'El directorio {path}{folder} no existe en la cuenta de Mega')
+                raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), self.destination_dir)
+        return folder_find[0]
+
     def upload_backup(self, filename_upload: str, user: str, password: str):
         # TODO: añadir línea al log (debug)
         print("Se inicia sesión en la cuenta de Mega")
@@ -22,14 +48,11 @@ class MegaBackupCreator(BackupCreator):
 
         # TODO: añadir línea al log (debug)
         print(f"Se busca el directorio {self.destination_dir} en la cuenta de Mega")
-        folder = mega.find(self.destination_dir, exclude_deleted=True)
-        # Comprobar si el directorio no existe, para crearlo
-        if not folder:
-            # TODO: añadir línea al log
-            print(f"Carpeta {self.destination_dir} no encontrada en la cuenta de Mega")
-            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), self.destination_dir)
+        id_folder: str = self.get_id_folder(mega=mega)
 
-        mega.upload(filename_upload, folder[0])
+        # TODO: añadir línea al log (debug)
+        print(f"Se procede a subir el fichero {filename_upload} a su cuenta de MEGA")
+        mega.upload(filename_upload, id_folder)
         # TODO: añadir línea al log
         print(f"El fichero {filename_upload} se ha subido a su cuenta de MEGA")
         remove(filename_upload)
